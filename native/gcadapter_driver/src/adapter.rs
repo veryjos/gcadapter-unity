54cd198cfaa3c7c9c832d7733bfb30e3c4e3a502
jos <jd325@njit.edu>
2019-02-09 16:04:15 -0500

    Move controller bits into adapter

--- native/gcadapter_driver/src/adapter.rs
+++ native/gcadapter_driver/src/adapter.rs
@@ -1,5 +1,33 @@
+use std::thread;
+use std::sync::Arc;
+
+use crate::sync_cell::SyncCell;
+
 use libusb;
 
-struct Adapter {
-    controllers: Vec<Controller>
+#[derive(Clone, Copy)]
+pub struct ControllerState {
+    plugged_in: bool,
+
+    buttons: u32,
+    x: f32,
+    y: f32,
+    cx: f32,
+    cy: f32
+}
+
+pub struct Adapter<'a> {
+    pub device: libusb::Device<'a>,
+
+    pub controllers: Vec<Arc<SyncCell<ControllerState>>>
+}
+
+impl<'a> Adapter<'a> {
+    pub fn new(device: libusb::Device<'a>) -> Adapter {
+        Adapter {
+            device,
+
+            controllers: vec!()
+        }
+    }
 }
--- native/gcadapter_driver/src/controller.rs
+++
@@ -1,9 +0,0 @@
-struct Controller {
-    plugged_in: bool,
-
-    buttons: u32,
-    x: f32,
-    y: f32,
-    cx: f32,
-    cy: f32
-}
